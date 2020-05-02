233b78bf43a53e8671929d0451a1eae6d339f1c4
unknown <parseshyam81@gmail.com>
2020-05-02 19:32:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+let { chrome } = window;
 function App() {
+  useEffect(() => {
+    chrome.runtime.onMessage.addListener(function (req, sender) {
+      chrome.storage.local.set({ 'address': req.address })
+      chrome.pageAction.show(sender.tab.id);
+      chrome.pageAction.setTitle({ tabId: sender.tab.id, title: req.address });
+    });
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>HEKKO</p>
     </div>
   );
 }
